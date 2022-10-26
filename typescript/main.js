c39bf8b2591df1aa4ef8ce4023c2527827f91f5f
Helin Ergezen <70849887+helinmelisa@users.noreply.github.com>
2022-10-26 21:35:37 +0300

    updates

--- typescript/main.js
+++ typescript/main.js
@@ -2,44 +2,26 @@
 // Servisler 
 // CRUD operasyonları Create Read Update Delete
 // GetAll, GetById, Add, Delete, Update
-// GetAll, GetById, Add, Delete, Update
-var ProductService = /** @class */ (function () {
-    function ProductService() {
-        this.products = [];
-    }
-    ProductService.prototype.add = function (product) {
-        this.products.push(product);
-    };
-    ProductService.prototype.getAll = function () {
-        return this.products;
-    };
-    ProductService.prototype.getById = function (id) {
-        return this.products.find(function (product) { return product.id === id; });
-        /*for (const product of this.products) {
-            if(product.id == id){
-                return product;
-            }
-        }*/
-    };
-    ProductService.prototype["delete"] = function (id) {
-        this.products = this.products.filter(function (product) { return !(product.id == id); });
-    };
-    ProductService.prototype.update = function (id, updatedProduct) {
-        var index = this.products.findIndex(function (product) { return (product.id == id); });
-        this.products.splice(index, 1, updatedProduct);
-    };
-    return ProductService;
-}());
+import { CategoryService } from "./services/categoryService.js";
+import { ProductService } from "./services/productService.js";
+// Bütün servisleri ve bütün aksiyonlarını main.ts'de test et.
+//ürün id, name, price
+//category id, name, desc
+var categoryService = new CategoryService();
 var productService = new ProductService();
-productService.add({ id: 1, name: "laptop", price: 100, category: { id: "kategori1", name: "teknoloji", desc: "elektronik" } });
-productService.add({ id: 2, name: "desktop", price: 150, category: { id: "kategori1", name: "teknoloji", desc: "elektronik" } });
-productService.add({ id: 3, name: "televizyon", price: 200, category: { id: "kategori1", name: "teknoloji", desc: "elektronik" } });
-productService.add({ id: 5, name: "telefon", price: 300, category: { id: "kategori1", name: "teknoloji", desc: "elektronik" } });
+categoryService.add({ id: 1, name: "teknoloji", desc: "elektronik" });
+categoryService.add({ id: 2, name: "mutfak", desc: "eşya" });
+var teknoloji = categoryService.getById(1);
+var mutfak = categoryService.getById(2);
+productService.add({ id: 1, name: "laptop", price: 100, category: teknoloji });
+productService.add({ id: 2, name: "fırın", price: 150, category: mutfak });
+productService.add({ id: 3, name: "televizyon", price: 200, category: teknoloji });
+productService.add({ id: 5, name: "buzdolabı", price: 300, category: mutfak });
 console.log(productService.getAll());
-//productService.delete(1)
+categoryService.update(1, { desc: "teknolojik ürünler" });
+productService.update(2, { name: "fırın+", price: 800 });
 console.log("**********");
-//console.log(productService.getAll());
-//console.log("**********");
-productService.update(2, { id: 8, name: "desktop++", price: 150, category: { id: "kategori1", name: "teknoloji", desc: "elektronik" } });
+console.log(productService.getAll());
 console.log("**********");
+productService["delete"](5);
 console.log(productService.getAll());
--- typescript/main.ts
+++ typescript/main.ts
@@ -3,62 +3,28 @@
 // CRUD operasyonları Create Read Update Delete
 // GetAll, GetById, Add, Delete, Update
 
-// Bütün servisleri ve bütün aksiyonlarını main.ts'de test et.
+import { CategoryService } from "./services/categoryService.js"
+import { ProductService } from "./services/productService.js"
 
+// Bütün servisleri ve bütün aksiyonlarını main.ts'de test et.
 //ürün id, name, price
 //category id, name, desc
 
-interface Product{
-    id: number;
-    name: string;
-    price: number;
-    category: Category;
-}
-interface Category{
-    id: string;
-    name: string;
-    desc: string;
-}
-// GetAll, GetById, Add, Delete, Update
-class ProductService{
-    products: Product[] = [];
-
-    add(product: Product){
-        this.products.push(product);
-    }
-
-    getAll():Product[]{
-        return this.products;
-    }
-
-    getById(id: number):Product|undefined{
-        return this.products.find(product => product.id === id);
-        /*for (const product of this.products) {
-            if(product.id == id){
-                return product;
-            }
-        }*/
-    }
-
-    delete(id: number) {
-        this.products = this.products.filter(product => !(product.id == id)); 
-    }
-
-    update(id: number, updatedProduct: Product) {
-       const index = this.products.findIndex(product => (product.id == id));
-       this.products.splice(index, 1, updatedProduct)
-    }
-}
-
-let productService = new ProductService()
-productService.add({id:1, name:"laptop", price:100, category:{id:"kategori1", name: "teknoloji", desc: "elektronik"}})
-productService.add({id:2, name:"desktop", price:150, category:{id:"kategori1", name: "teknoloji", desc: "elektronik"}})
-productService.add({id:3, name:"televizyon", price:200, category:{id:"kategori1", name: "teknoloji", desc: "elektronik"}})
-productService.add({id:5, name:"telefon", price:300, category:{id:"kategori1", name: "teknoloji", desc: "elektronik"}})
+const categoryService = new CategoryService()
+const productService = new ProductService()
+categoryService.add({id:1, name: "teknoloji", desc: "elektronik"})
+categoryService.add({id:2, name: "mutfak", desc: "eşya"})
+const teknoloji = categoryService.getById(1)
+const mutfak = categoryService.getById(2)
+productService.add({id:1, name:"laptop", price:100, category:teknoloji })
+productService.add({id:2, name:"fırın", price:150, category:mutfak})
+productService.add({id:3, name:"televizyon", price:200, category:teknoloji})
+productService.add({id:5, name:"buzdolabı", price:300, category:mutfak})
 console.log(productService.getAll());
-//productService.delete(1)
-//console.log(productService.getAll());
-//console.log("**********");
-productService.update(2,{id:8, name:"desktop++", price:150, category:{id:"kategori1", name: "teknoloji", desc: "elektronik"}});
+categoryService.update(1,{desc:"teknolojik ürünler"})
+productService.update(2,{name:"fırın+", price:800});
 console.log("**********");
+console.log(productService.getAll());
+console.log("**********");
+productService.delete(5)
 console.log(productService.getAll());
